2b5bac939e4f6212367cf6f5aac5673bf14dc527
Ines Arous <ines@exascale.info>
2018-06-11 12:39:40 +0200

    readme

--- cd_cluster.py
+++ cd_cluster.py
@@ -60,41 +60,21 @@ def CCD(x,n,m,k):
 	    classes=classes+(2**(i))*(E[:,i].reshape(n,1))  
 	return classes
 
-input_fashion=pd.read_csv('/Volumes/GoogleDrive/My Drive/Phd/data/Fashion/Zalando_data_M3/modified_data/timeseries_nitems/region1_fs.csv')
-best_combination=pd.concat([input_fashion.iloc[:,1],input_fashion.iloc[:,4],input_fashion.iloc[:,6],input_fashion.iloc[:,8]],axis=1)
-input_matrix=stats.zscore(best_combination.values)
-#b, a = signal.butter(4, 0.001)
-#zi = signal.lfilter_zi(b, a)
-#z= signal.lfilter(b, a, input_matrix)
 
-#input_matrix=savgol_filter(input_matrix1, window_length=3, polyorder=2, mode='nearest')
-classes=CCD(input_matrix,input_matrix.shape[0],input_matrix.shape[1],3)
-df=pd.concat([pd.DataFrame(data=input_matrix),pd.DataFrame(data=classes,columns=['ccd'])], axis=1)
+# Define main method that calls other functions
+def main():
+	inputfile=sys.argv[1]       #'./Input/example.txt'
+ 	outputfile=sys.argv[2]     #'./Result/classes.txt'  
+ 	n =int(sys.argv[3])                #input_matrix.shape[0]
+	m =int(sys.argv[4])                 #input_matrix.shape[1]
+	k=int(sys.argv[5])                  #k=2    
+	input_matrix=np.loadtxt(inputfile,delimiter=' ')
+	input_matrix=stats.zscore(input_matrix)
+	#classify the matrix using CD
+	classes=CCD(input_matrix,n,m,k)
+	np.savetxt(outputfile,classes,fmt='%.2f')
+    
 
-fig=plt.figure(figsize=(20,10))
-plt.plot(df,linestyle='solid',marker='None')
-plt.ylabel('# items', fontsize=20)
-plt.xlabel('timestamps', fontsize=20)
-fig.suptitle('Fashion time series', fontsize=20)
-ax1 = df.cumsum().plot()
-lines, labels = ax1.get_legend_handles_labels()
-fig.legend(lines[:5], labels[:5], loc='best')  # legend for first two lines only
-plt.ion()
-plt.show()
-## Define main method that calls other functions
-#def main():
-#	inputfile=sys.argv[1]       #'./Input/example.txt'
-# 	outputfile=sys.argv[2]     #'./Result/classes.txt'  
-# 	n =int(sys.argv[3])                #input_matrix.shape[0]
-#	m =int(sys.argv[4])                 #input_matrix.shape[1]
-#	k=int(sys.argv[5])                  #k=2    
-#	input_matrix=np.loadtxt(inputfile,delimiter=' ')
-#	#input_matrix=stats.zscore(input_matrix)
-#	#classify the matrix using CD
-#	classes=CCD(input_matrix,n,m,k)
-#	np.savetxt(outputfile,classes,fmt='%.2f')
-#    
-#
-## Execute main() function
-#if __name__ == '__main__':
-#    main()
+# Execute main() function
+if __name__ == '__main__':
+    main()
