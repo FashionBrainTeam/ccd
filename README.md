c38389149336c68e6322b8170384b16d2869a02d
Ines Arous <ines@exascale.info>
2018-06-11 12:37:58 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# ccd
+# CCD: Classification of fashion time series
+
+EU project 732328: "Fashion Brain".
+
+D5.4: " The classification algorithm and its evaluation on fashion time series".
+
+## Getting started
+
+### Installation:
+``` bash 
+git clone https://github.com/eXascaleInfolab/ccd.git}
+cd ccd/
+```
+
+### Description of the "CCD" package
+The ”CCD” package contains the following files:
+- cd cluster.py: the implementation of CCD algorithm
+- Input folder: This folder contains the running example input matrix in the file ”example.txt”
+- Result folder:This folder contains the results of the CCD on the running example 
+
+ 
+
+### Running the code 
+In order to run an experiment, the cd cluster.py file is used. The arguments needed
+for the cd cluster.py file are the following:
+- The path for the input file
+- The path for the output file
+- # of rows n, which takes any integer number
+- # of columns m, which takes any integer number
+- # of truncated columns k, which takes any integer number and needs to be less than m
+
+
+Example: To run an experiment of CCD with the following parameters:
+- input file=./Input/example.txt
+- output file= ./Result/classes.txt
+- #rows= 8
+- #cols=4
+- #truncated cols = 2
+the corresponding command line would be the following:
+    ``` bash 
+       python cd_cluster.py ./Input/example.txt ./Result/classes.txt 8 4 2
+    ```
--- cd_cluster.py
+++ cd_cluster.py
@@ -9,6 +9,7 @@ Created on Sun Jun 10 14:45:03 2018
 import numpy as np
 import sys
 import pandas as pd
+from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 #from sklearn.cluster import KMeans
@@ -59,21 +60,41 @@ def CCD(x,n,m,k):
 	    classes=classes+(2**(i))*(E[:,i].reshape(n,1))  
 	return classes
 
+input_fashion=pd.read_csv('/Volumes/GoogleDrive/My Drive/Phd/data/Fashion/Zalando_data_M3/modified_data/timeseries_nitems/region1_fs.csv')
+best_combination=pd.concat([input_fashion.iloc[:,1],input_fashion.iloc[:,4],input_fashion.iloc[:,6],input_fashion.iloc[:,8]],axis=1)
+input_matrix=stats.zscore(best_combination.values)
+#b, a = signal.butter(4, 0.001)
+#zi = signal.lfilter_zi(b, a)
+#z= signal.lfilter(b, a, input_matrix)
 
-# Define main method that calls other functions
-def main():
-	inputfile=sys.argv[1]       #'./Input/example.txt'
- 	outputfile=sys.argv[2]     #'./Result/classes.txt'  
- 	n =int(sys.argv[3])                #input_matrix.shape[0]
-	m =int(sys.argv[4])                 #input_matrix.shape[1]
-	k=int(sys.argv[5])                  #k=2    
-	input_matrix=np.loadtxt(inputfile,delimiter=' ')
-	#input_matrix=stats.zscore(input_matrix)
-	#classify the matrix using CD
-	classes=CCD(input_matrix,n,m,k)
-	np.savetxt(outputfile,classes,fmt='%.2f')
-    
+#input_matrix=savgol_filter(input_matrix1, window_length=3, polyorder=2, mode='nearest')
+classes=CCD(input_matrix,input_matrix.shape[0],input_matrix.shape[1],3)
+df=pd.concat([pd.DataFrame(data=input_matrix),pd.DataFrame(data=classes,columns=['ccd'])], axis=1)
 
-# Execute main() function
-if __name__ == '__main__':
-    main()
+fig=plt.figure(figsize=(20,10))
+plt.plot(df,linestyle='solid',marker='None')
+plt.ylabel('# items', fontsize=20)
+plt.xlabel('timestamps', fontsize=20)
+fig.suptitle('Fashion time series', fontsize=20)
+ax1 = df.cumsum().plot()
+lines, labels = ax1.get_legend_handles_labels()
+fig.legend(lines[:5], labels[:5], loc='best')  # legend for first two lines only
+plt.ion()
+plt.show()
+## Define main method that calls other functions
+#def main():
+#	inputfile=sys.argv[1]       #'./Input/example.txt'
+# 	outputfile=sys.argv[2]     #'./Result/classes.txt'  
+# 	n =int(sys.argv[3])                #input_matrix.shape[0]
+#	m =int(sys.argv[4])                 #input_matrix.shape[1]
+#	k=int(sys.argv[5])                  #k=2    
+#	input_matrix=np.loadtxt(inputfile,delimiter=' ')
+#	#input_matrix=stats.zscore(input_matrix)
+#	#classify the matrix using CD
+#	classes=CCD(input_matrix,n,m,k)
+#	np.savetxt(outputfile,classes,fmt='%.2f')
+#    
+#
+## Execute main() function
+#if __name__ == '__main__':
+#    main()
